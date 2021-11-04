45c5f809736f91eac222d087d16df9df06d107e0
Ali Kaner <53481701+AliKaner@users.noreply.github.com>
2021-11-04 14:19:36 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {QuestionCard} from "./components/question/question_card";
+
+const TOTAL_QUESTIONS = 10;
 export const  App = () =>{
+  const [loading,setLoading] = useState(false);
+  const [questions,setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setAnswers] = useState([]);
+  const [score,setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true)
   const startTravia = async () => {
 
   }
@@ -18,7 +26,14 @@ export const  App = () =>{
     </button>
     <p className="score">Score:</p>
     <p>Loading Questions...</p>
-    <QuestionCard/>
+    <QuestionCard
+      questionNumber={number + 1}
+      totalQuestions={TOTAL_QUESTIONS}
+      question={questions[number].question}
+      answers={questions[number].answers}
+      userAnswer={userAnswers ? userAnswers[number]:undefined}
+      callback={checkAnswer}
+    />
     <button className="next" onClick={nextQuestion}>
       Next Question
     </button>
